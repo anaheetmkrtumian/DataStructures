e416dfa221b3046e67e06acbde02abc4aa655d06
anaheetmkrtumian <anaheetmkrtumian@gmail.com>
2026-03-14 17:57:15 +0400

    stack and queue

---
+++ MyStackProj/Stack.cs
@@ -0,0 +1,31 @@
+﻿using MyLinkedListLib;
+using System;
+
+namespace MyStackProj;
+
+public class MyStack<T>
+{
+    private MyLinkedList<T> _items = new MyLinkedList<T>();
+
+    public int Count
+    {
+        get { return _items.Count; }
+    }
+
+    public void Push(T item)
+    {
+        _items.AddFirst(item);
+    }
+
+    public T Pop()
+    {
+        T value = _items.First();
+        _items.RemoveFirst();
+        return value;
+    }
+
+    public T Peek()
+    {
+        return _items.First();
+    }
+}
