b9b739f53e4f345682faad8e9702cf26a7ef3588
anahetmkrtumian <anaheet.mkrtumian@gmail.com>
2026-04-23 10:44:09 +0400

    push

---
+++ MySelectionSortProject/MySelectionSort.cs
@@ -0,0 +1,34 @@
+﻿namespace MySelectionSortProject
+{
+    public class MySelectionSort<T>
+        where T : IComparable<T>
+    {
+        public void Sort(T[] items)
+        {
+            for (int i = 0; i < items.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < items.Length; j++)
+                {
+                    if (Compare(items[j], items[minIndex]) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+                swap(items, i, minIndex);
+            }
+        }
+
+        private void swap(T[] items, int i, int minIndex)
+        {
+            T temp = items[i];
+            items[i] = items[minIndex];
+            items[minIndex] = temp;
+        }
+
+        private int Compare(T t1, T t2)
+        {
+            return t1.CompareTo(t2);
+        }
+    }
+}
